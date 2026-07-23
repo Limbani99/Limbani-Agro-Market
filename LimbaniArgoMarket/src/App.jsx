6b08727e0132ea1f9aafeb68ad35662d81265f35
mannlimbani <Patelmann2424@gmail.com>
2026-07-23 17:03:52 +0530

    feat: implement global Tailwind theme configuration and responsive navigation bar component

--- LimbaniArgoMarket/src/App.jsx
+++ LimbaniArgoMarket/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Layout from './layout/Layout'
 import Home from './page/Home'
 import Categories from './page/Categories'
+import Dealers from './page/Dealers'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route element={<Layout />}>
           <Route path='/' element={<Home />} />
           <Route path='/categories' element={<Categories />} />
+          <Route path='/dealers' element={<Dealers />} />
         </Route>
       </Routes>
     </>
